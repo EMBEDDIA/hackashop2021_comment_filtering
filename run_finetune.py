ef81034f357b13d3873f6d6fe1c991e0a2756ad8
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2021-01-24 17:21:18 +0000

    1st

--- run_finetune.py
+++ run_finetune.py
@@ -5,22 +5,23 @@ import sys
 
 import subprocess
 
-base_out_dirs ={"all":"./models/all/",
+base_out_dirs ={
+                "all":"./models/all/",
                "cse":"./models/cse/",
                "engee":"./models/engee/"
                 }
 
 language_data = {
                 "all": {"data": "text", "label": "label", "train_data_path": "./data/all_train.csv",
-                           "eval_data_path": "./data/all_val.csv", "test_data_path": "./data/all_test.csv",
+                           "eval_data_path": "./data/all_val.csv",
                            "offensive_label": "OFF"},
                 "cse": {"data": "text", "label": "label", "train_data_path": "./data/cse_train.csv",
-                                           "eval_data_path": "./data/cse_val.csv", "test_data_path": "./data/cse_test.csv",
+                                           "eval_data_path": "./data/cse_val.csv",
                                            "offensive_label": "OFF"},
 
                 "engee": {"data": "text", "label": "label", "train_data_path": "./data/engee_train.csv",
-                                           # "eval_data_path": "../data/engee_val.csv", "test_data_path": "egnee_test.csv",
-                                            "eval_data_path": "./data/engee_train.csv", "test_data_path": "./data/engee_train.csv",
+                                           "eval_data_path": "../data/engee_val.csv",
+
                                            "offensive_label": "OFF"}
 
                 }
@@ -60,7 +61,7 @@ if __name__ == "__main__":
 
     list_arg = [python_path, "finetune_bert.py",
                 "--train_data_path", data_details['train_data_path'],
-                "--test_data_path", data_details['test_data_path'],
+
                 "--eval_data_path", data_details['eval_data_path'],
                 "--output_dir", out_dir,
                 "--data_column", data_details['data'],
@@ -69,57 +70,3 @@ if __name__ == "__main__":
                 ]
     subprocess.call(list_arg)
 
-    # for pre_train_lang in pre_train_langs:
-    #     model_details = int_model_files[pre_train_lang][model]
-    #     base_out_dir = base_out_dirs[pre_train_lang]
-    #
-    #     langs = ['arabic', 'german','slovenian', 'croatian','english']
-    #     seeds =  ['42', '84', '126']
-    #     runs = ['1', '2', '3']
-    #
-    #     for lang in langs:
-    #         #No task traning on same as pre-training language
-    #         if lang == pre_train_lang:
-    #             continue
-    #         data_details = language_data[lang]
-    #
-    #         for run, seed in zip(runs, seeds):
-    #             out_dir = base_out_dir + model + '_'+lang+run+'/'
-    #
-    #             if len(model_details['model_file']) == 0:
-    #                 list_arg = [python_path, "incremental_learning.py",
-    #                             "--train_data_path", data_details['train_data_path'],
-    #                             "--test_data_path", data_details['test_data_path'],
-    #                             "--eval_data_path", data_details['eval_data_path'],
-    #                             "--output_dir", out_dir,
-    #                             "--data_column", data_details['data'],
-    #                             "--label_column", data_details['label'],
-    #                             "--random_seed", seed]
-    #
-    #             elif len(model_details['tokenizer_file']) == 0:
-    #
-    #                 list_arg = [python_path, "incremental_learning.py",
-    #                                  "--train_data_path", data_details['train_data_path'],
-    #                                  "--test_data_path", data_details['test_data_path'],
-    #                                  "--eval_data_path", data_details['eval_data_path'],
-    #                                  "--output_dir", out_dir,
-    #                                  "--data_column", data_details['data'],
-    #                                  "--label_column", data_details['label'],
-    #                                  "--config_file", model_details['config_file'],
-    #                                  "--model_file", model_details['model_file'],
-    #                                  "--random_seed", seed]
-    #             else:
-    #                 list_arg = [python_path, "incremental_learning.py",
-    #                             "--train_data_path", data_details['train_data_path'],
-    #                             "--test_data_path", data_details['test_data_path'],
-    #                             "--eval_data_path", data_details['eval_data_path'],
-    #                             "--output_dir", out_dir,
-    #                             "--data_column", data_details['data'],
-    #                             "--label_column", data_details['label'],
-    #                             "--tokenizer_file",model_details['tokenizer_file'],
-    #                             "--config_file", model_details['config_file'],
-    #                             "--model_file", model_details['model_file'],
-    #                             "--random_seed", seed]
-    #             print(list_arg)
-    #
-    #             subprocess.call(list_arg)
